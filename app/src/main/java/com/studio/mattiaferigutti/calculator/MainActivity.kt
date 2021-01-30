233faa77de66f8162cbcd4cd24cc7e29dd1ae5ac
Mattia Ferigutti <mattiaferigutti@gmail.com>
2021-01-30 19:16:15 +0100

    connecting the UI with Kotlin code

--- app/src/main/java/com/studio/mattiaferigutti/calculator/MainActivity.kt
+++ app/src/main/java/com/studio/mattiaferigutti/calculator/MainActivity.kt
@@ -2,10 +2,65 @@ package com.studio.mattiaferigutti.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onItemClick(view: View) {
+        when (view.id) {
+            R.id.button0 -> {
+
+            }
+            R.id.button1 -> {
+
+            }
+            R.id.button2 -> {
+
+            }
+            R.id.button3 -> {
+
+            }
+            R.id.button4 -> {
+
+            }
+            R.id.button5 -> {
+
+            }
+            R.id.button6 -> {
+
+            }
+            R.id.button7 -> {
+
+            }
+            R.id.button8 -> {
+
+            }
+            R.id.button9 -> {
+
+            }
+            R.id.addButton -> {
+
+            }
+            R.id.minusButton -> {
+
+            }
+            R.id.multiplyButton -> {
+
+            }
+            R.id.divideButton -> {
+
+            }
+            R.id.pointButton -> {
+
+            }
+            R.id.invertButton -> {
+
+            }
+        }
     }
 }
